e397f611c7031f5b45ee25eb839991cd1d660506
tenlie <ten_lie_10@daum.net>
2016-10-22 09:59:41 +0900

    Observer 이동에 따른 시야 회전

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,38 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PathFollower : MonoBehaviour
 {
     public Rigidbody2D rb2D { get; set; }
-    
-    public Transform[] path;
-    public float speed = 5.0f;
-    public float reachDist;
-    public int currentPoint = 0;
+    public Vector3 prevPos;
 
     // Use this for initialization
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        prevPos = transform.position;
+        StartCoroutine("RotateObserver");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RotateObserver()
     {
-        //  Vector3 direction = path[currentPoint].position - transform.position; 
-        float distance = Vector3.Distance(path[currentPoint].position, transform.position);
-        Vector3 newPosition = Vector3.MoveTowards(transform.position, path[currentPoint].position, Time.smoothDeltaTime * speed);
-        rb2D.MovePosition(newPosition);
-
-        Vector3 direction = path[currentPoint].position - transform.position;
-        direction.Normalize();
-        float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
-
-        if (distance <= reachDist)
-            currentPoint++;
-
-        if (currentPoint >= path.Length)
-            currentPoint = 0;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+            Vector3 direction = transform.position - prevPos;
+            direction.Normalize();
+            float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+            prevPos = transform.position;
+        }
     }
 
     /*
