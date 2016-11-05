3614622030a0948aa92fc95cd9f5af502eed7e51
JangMS <imsochangable@gmail.com>
2016-11-05 10:46:41 +0900

    적 시야발각시 아이콘 발동, 적 접촉시 사망판정, 프린터 접근시 Deathnumber 초기화

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -8,6 +8,7 @@ public class FieldOfView : MonoBehaviour {
     [Range(0, 360)]
     public float viewAngle;
     public GameObject GameOverUI_BG;
+    public GameObject WarningMark;
 
 
     public LayerMask targetMask;
@@ -37,7 +38,7 @@ public class FieldOfView : MonoBehaviour {
     void FindVisibleTargets()
     {
         visibleTargets.Clear();
-
+        WarningMark.SetActive(false);
        
         
         //TODO : Length Debugging
@@ -55,7 +56,8 @@ public class FieldOfView : MonoBehaviour {
 
                 if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
                 {
-                
+
+                    WarningMark.SetActive(true);
                     visibleTargets.Add(target);
                     GameOver();
 
